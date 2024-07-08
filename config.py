4c148ea0ba2ffd22865893a4c33cf5287300c421
Manu1wybggsn <saurabh112dwivedi@gmail.com>
2024-07-08 19:48:28 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6775811275:AAEoE5BrrFIAdnhqs8B4LpMhDBBkKEhSOrk")
+    API_ID =int(os.environ["API_ID", 20778821
+    ]
+    API_HASH = os.environ["API_HASH", "70ddbf0162bafe8b7e0007c3b22d01c0"]
+    AUTH_USERS = "6236239489"
+    AUTH_GROUP = "-1001577274993"""
+
